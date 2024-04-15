b07a9b220dd2152a5d3aaab9ed7234f00201b3b3
easley <nevermore.wd@me.com>
2024-04-15 09:49:41 +0800

    porting to .NET8

---
+++ WindowsInput.Test/UnicodeText/UnicodeTestForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsInput.Tests.UnicodeText
+{
+    public partial class UnicodeTestForm : Form
+    {
+        public UnicodeTestForm()
+        {
+            InitializeComponent();
+        }
+
+        public string Expected
+        {
+            get { return ExpectedTextBox.Text; }
+            set { ExpectedTextBox.Text = value; }
+        }
+
+        public string Recieved { get { return RecievedTextBox.Text; } }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            RecievedTextBox.Focus();
+        }
+    }
+}
--- WindowsInput/Properties/AssemblyInfo.cs
+++ WindowsInput/Properties/AssemblyInfo.cs
@@ -2,20 +2,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("WindowsInput")]
-[assembly: AssemblyDescription("Provides a useful wrapper around the User32 SendInput and related native Windows functions for simulating Input Devices in Windows including Keyboard and Mouse.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("michaelnoonan")]
-[assembly: AssemblyProduct("WindowsInput")]
-[assembly: AssemblyCopyright("Copyright © michaelnoonan 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("WindowsInput.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100737a78808b105748ff1251c1eb68752c2eb02dea580cade31d04fbc4081d7f420c639de2f3081461f734d48499c50133adb9dab02d828031cdec7d2941652233aefadf961658f948c13d43da9860d76ded6e959eebec0ddae03cc67dabf1aa83973602690e228c5b2d9434e62760d325d4c46a1fdf71971bb7a22f0de55b7bf0")]
+[assembly: InternalsVisibleTo("WindowsInput.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100459d6e59794b1a45444d2dd06713603ce23f10cac2338aea857487da414b1812278196bbeb5d3b5546ccb1d9191d47a027fc04c8641c839bdffc012b4f492c2c5015e1497fb6d9c90bf05f029bc6cda953ee522b5935ff34dff1b3fcfaaa8f3d0c42837b9ad639f180e3f2fbdd6e3efca29e0b6d0ace913edbacb6ced00509d8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -24,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("25871eb0-27d6-42da-a3fe-cb60eb01f15a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
